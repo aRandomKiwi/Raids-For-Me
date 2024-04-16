a6d2a4d89049410cfe5a0f9743e2669903b58175
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2024-04-16 09:14:06 +0200

    Add Rimworld 1.5 support

---
+++ Source/1.5/Harmony/Patches.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace aRandomKiwi.KFM
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var inst = new Harmony("rimworld.randomKiwi.RFM");
+            inst.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static FieldInfo MapFieldInfo;
+    }
+}
